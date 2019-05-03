ee21e13144be06427bb2e888999b89c89de68a8d
LIPermana <irmanpermana.lu@gmail.com>
2019-05-03 18:40:56 +0700

    proses ubah data

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -48,32 +48,32 @@ class FormController extends Controller
     return redirect('/view');
   }
 
-  // public function edit($id)
-  // {
-  //   $buku = Buku::find($id);
-  //
-  //   return view('form', compact('buku'));
-  // }
-  //
-  // public function update(Request $request, $id)
-  // {
-  //   $request->validate([
-  //     'judul'=>'required',
-  //     'penerbit'=>'required',
-  //     'tahun_terbit'=>'required|integer',
-  //     'pengarang'=>'required'
-  //   ]);
-  //
-  //   $buku = Buku::find($id);
-  //   $buku->judul = $request->get('judul');
-  //   $buku->penerbit = $request->get('penerbit');
-  //   $buku->tahun_terbit = $request->get('tahun_terbit');
-  //   $buku->pengarang = $request->get('pengarang');
-  //   $buku->save();
-  //
-  //   // dd($request);
-  //
-  //   return redirect('/view')->with('Success','Data buku telah diperbarui');
-  // }
+  public function edit($id)
+  {
+    $buku = Buku::find($id);
+
+    return view('form', compact('buku'));
+  }
+
+  public function update(Request $request, $id)
+  {
+    $request->validate([
+      'judul'=>'required',
+      'penerbit'=>'required',
+      'tahun_terbit'=>'required|integer',
+      'pengarang'=>'required'
+    ]);
+
+    $buku = Buku::find($id);
+    $buku->judul = $request->get('judul');
+    $buku->penerbit = $request->get('penerbit');
+    $buku->tahun_terbit = $request->get('tahun_terbit');
+    $buku->pengarang = $request->get('pengarang');
+    $buku->save();
+
+    // dd($request);
+
+    return redirect('/view')->with('Success','Data buku telah diperbarui');
+  }
 
 }
